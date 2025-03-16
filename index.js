1268cab3eff9895e4942b133852ecc81a2b6b3db
Luis Casas <zs21013875@estudiantes.uv.mx>
2025-03-15 20:40:18 -0600

    ORM configuration

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 dotenv.config();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+	res.send('Hello World');
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.SERVER_PORT}`);
+	console.log(`Server running on port ${process.env.SERVER_PORT}`);
 });
